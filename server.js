0a7aa4bec2cc96014096bb089ea88b65f21baa6e
Gabriel Penteado <gabripenteado@gmail.com>
2023-05-25 00:52:58 -0300

    added CORS

--- server.js
+++ server.js
@@ -2,9 +2,23 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes');
+const cors = require('cors');
 
 //____________CONFIG______________
 const PORT = process.env.PORT || 8000;
+
+// CORS enable for all origins
+app.use(cors());
+
+// CORS CONFIG FOR A UNIQUE ORIGIN
+//  const corsOptions = {
+//   "origin": "*",
+//   "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+//   "preflightContinue": false,
+//   "optionsSuccessStatus": 204
+// };
+//
+//app.use(cors(corsOptions));
 
 // PARSE INCOMING DATA AS JSON
 app.use(express.json({ limit: '1mb' }));
